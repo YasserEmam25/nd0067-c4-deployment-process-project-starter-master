77848b629b4b76e3bc37f994d7092e2107fb2f6d
Yasser Emam <YasserEmam2002@gmail.com>
2022-08-04 18:35:12 +0200

    deployed the code to aws

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost:
+    "http://udagram-env-4.eba-4e4fikfa.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
